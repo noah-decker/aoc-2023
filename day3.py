7cd9f12106cb1762bddb7e9b8b25c1d984197030
ndecker2020 <ndecker@gustavus.edu>
2023-12-12 19:39:05 -0600

    giving up on day 3

--- day3.py
+++ day3.py
@@ -1,94 +1,88 @@
 with open('inputs\day3_input.txt') as f:
     text = f.read()
-text = '''000..000..
+atext = '''467..114..
 ...*......
-..00..000.
+..35..633.
 ......#...
-000*......
-.....+.00.
-..000.....
-......000.
-....$.....
-.100.100..
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
 '''
-def get_digits(word,index):
-    start = index
-    end = index
-    if not word[index].isnumeric():
-        return ''
-    while True:
-        if word[start-1].isnumeric():
-            start = start - 1
-        elif end<len(word)-1 and word[end+1].isnumeric():
-            end = end+1
-        else:
-            break
-    return word[start:end+1]
-def get_indexs(current_index):
-    if current_index == 0:
-        return [current_index,current_index+1]
-    else:
-        return [current_index-1,current_index,current_index+1]
-data = dict(zip(range(len(text.split('\n'))),text.split('\n')))
-new_data = {x:[] for x in range(len(text.split('\n')))}
-#print(get_digits('467..114..',5))
-#print(get_digits('467..114..',6))
-#print(data)
-other_dict = {}
-for key in data:
-    for index in range(len(data[key])):
-        char = data[key][index]
-        if char in "!@#$%^&*()\{\}[]=+:;<>,`~\\?/|_-'\"":
-            checklist = get_indexs(index)
-            for check in checklist:
-                if key>0:
-                    above = get_digits(data[key-1],check)
-                    if above!='':
-                        new_data[key-1].append(int(above))
-                        if int(above) in other_dict:
-                            other_dict[int(above)].append((key-1,check))
-                        else:
-                            other_dict[int(above)]=[(key-1,check)]
-                if key!=len(data)-1:
-                    below = get_digits(data[key+1],check)
-                    if below!='':
-                        new_data[key+1].append(int(below))
-                        if int(below) in other_dict:
-                            other_dict[int(below)].append((key+1,check))
-                        else:
-                            other_dict[int(below)]=[(key+1,check)]
-                if check != index:
-                    same_row = get_digits(data[key],check)
-                    if same_row!='':
-                        new_data[key].append(int(same_row))
-                        if int(same_row) in other_dict:
-                            other_dict[int(same_row)].append((key,check))
-                        else:
-                            other_dict[int(same_row)]=[(key,check)]
-final_data = {key:set(value) for key,value in new_data.items()}
 
-total = 0
-for key in final_data:
-    for num in final_data[key]:
-        total = total + num
-second = 0
+special_char = "!@#$%^&*()\{\}[]=+:;<>,`~\\?/|_-'\""
+alist = text.split("\n")
+alist.pop()
+strlen = len(alist[0])
+listlen = len(alist)
+def find_num(j,string):
+    if string[j].isnumeric():
 
-print(other_dict)
-for key,value in other_dict.items():
-    if len(value)==1:
-        second = second + key
-    else:
-        row = -1
-        index = -1
-        for item in value:
-            if item[0]==row and (index+1==item[1] or index-1==item[1]):
-                continue
+        start = j
+        end = j
+        while True:
+            if start>0 and string[start-1].isnumeric():
+                start = start-1
+            elif end<=len(string)-2 and string[end+1].isnumeric():
+                end = end + 1
             else:
-                second = second + key
-            row = item[0]
-            index = item[1]
+                break
+        return string[start:end+1],start, end
+    else:
+        return ''
+def make_index_range(string,start,end):
+    start = start-1 if start-1>=0 else 0
+    end = end+1 if end+1<=len(string)-1 else len(string)-1
+    return [x for x in range(start,end+1)]
+def check_symbols(strlist,list_index,start,end):
+    string = strlist[list_index]
+    index_range = make_index_range(string,start,end)
+    #print('cs',list_index,start,end)
+    if (start>0 and string[start-1] in special_char):
+        return True
+    elif(end<len(string)-1 and string[end+1] in special_char):
+        return True
+    else: 
+        if list_index == 0:
+            for i in index_range:
+                if strlist[list_index+1][i] in special_char:
+                    return True
+            return False
+        elif list_index == len(strlist)-1:
+            for i in index_range:
+                if strlist[list_index-1][i] in special_char:
+                    return True
+            return False
+        else:
+            for i in index_range:
+                #print(list_index,i,strlist)
+                if strlist[list_index+1][i] in special_char or strlist[list_index-1][i] in special_char:
+                    return True
+            return False
+total = 0
+print(listlen,strlen)
+for list_index in range(0,listlen):
+    end = -1
+    for str_index in range(0,strlen):
+        print(str_index , f'end: {end}  {alist[list_index][str_index]}')
+        if str_index > end:
+            #print(f'end = {end}')
+            print(f'end: {end}  {alist[list_index][str_index]}')
+            if (alist[list_index][str_index]).isnumeric():
+                string, start, temp_end = find_num(str_index,alist[list_index])
+                #print(list_index,start,end,temp_end)
+                print(f'number: {string} cs: {check_symbols(alist,list_index,start,temp_end)}')
+                if check_symbols(alist,list_index,start,temp_end):
+                    print(int(string))
+                    total = total + int(string)
+                    end = temp_end
+        else:
+            continue
+print(total)                
 
-print(total)
-print(second)
+
+                
 
 
