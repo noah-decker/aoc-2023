5c642b1934de75ffa8c584e163d30e0560eb7e03
ndecker2020 <ndecker@gustavus.edu>
2023-12-21 19:28:45 -0600

    finish day 4

--- day4.py
+++ day4.py
@@ -1,6 +1,6 @@
 with open('inputs\day4_input.txt') as f:
     text = f.read()
-text = '''Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+atext = '''Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
@@ -8,7 +8,7 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'''
 """
 a = {1:1,2:1,3:1,4:1}
->>> for key in a:
+for key in a:
 	matches = 3
 	for i in range(key+1,key+matches+1):
 		a[i] = a[i]+a[key]*matches
@@ -43,10 +43,17 @@ def score_game(win_num,my_num):
             count = count + 1
     return count
 score = 0 
-for line in alist:
-    line = line[line.find(':')+1:].split('|')
-    game_score = score_game(line[0],line[1])
-    score = score + game_score
+for key,value in adict.items():
+    nums = value[0].split('|')
+    w_num = nums[0]
+    m_num = nums[1]
+    count = score_game(w_num,m_num)
+    for i in range(key+1,key+count+1):
+        if i in adict:
+            adict[i][1] = adict[i][1]+1*(adict[key][1])
+print(adict)
+for key in adict:
+    score = score + adict[key][1]
 print(score)
 
 #part 1
